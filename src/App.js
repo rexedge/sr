03646e7f27c4f147370bfa953005e8be6d54f5bc
Rexedge <iscedigitalconcept@gmail.com>
2022-12-09 07:32:33 +0100

    pushing to github

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
-import { RXContainer } from './lib/components/layout';
-import Btn from './lib/components/RXBtn';
-import TextInput from './lib/components/RXInput';
+import './lib/styles/striferral.css';
 
 function App() {
 	return (
 		<div className="App">
-			<RXContainer flex screen centered dark>
+			<div className="container">
 				<div className="text-center">
-					<img src='/assets/images/sampleImage.jpg' width={150} className="rounded" alt='' />
 					<div>
-						Welcome to StriFerral
-					</div>
-					<TextInput
-						type='text'
-						placeholder='Small Text'
-					/>
-					<TextInput
-						type='textarea'
-						placeholder='Large Text'
-					/>
-					<div className='d-grid'>
-						<Btn variant={'success'}
-							tag='button'
-							onClick={() => alert("clicked")}
-							icon='whatsapp'
-							size={'sm'}
-							className='mb-2'
-						>
-							Do Something
-						</Btn>
-						<Btn variant={'danger'}
-							tag='a'
-							link={'#go_somwhere'}
-							icon='forward'
-							size={'sm'}
-						>
-							Go Somewhere
-						</Btn>
+						Welcome to Rex User Interface
 					</div>
 				</div>
-			</RXContainer>
+			</div>
 		</div>
 	);
 }
---
+++ src/lib/components/RXImg.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Box = styled.div`
+     width: ${props => 
+          props.size === 'sm'
+          ? '150px' :
+          props.size === 'md'
+          ? '210px' :
+          props.size === 'lg'
+          ? '300px' :
+          props.size === 'full'
+          ? '100%' :
+          props.size
+     };
+     height: ${props => 
+          props.square && props.size === 'sm'
+          ? '150px' :
+          props.square && props.size === 'md'
+          ? '210px' :
+          props.square && props.size === 'lg'
+          ? '300px' :
+          props.square && props.size === 'full'
+          ? '100%' :
+          props.size
+     };
+     overflow: hidden;
+     border-radius: ${props => 
+          props.square && props.circle
+          ? '50%' :
+          '10px'
+     };
+`
+
+const Img = styled.img`
+     object-fit: cover;
+     object-position: center;
+     width: 100%;
+     height: 100%;
+`
+
+export default function RXImg({
+
+     src = '/assets/images/sampleImage.jpg',
+     className = '',
+     size = 'sm',
+     square,
+     circle,
+
+}) {
+     return (
+          <Box 
+               className={className + ' mb-2'}
+               size={size}
+               square={square}
+               circle={circle}
+          >
+               <Img src={src} alt='' />
+          </Box>
+     )
+}
